45c6656757f1880a2ce4f4b606a752fe5fb1d740
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2013-11-02 00:44:58 +0100

    add echoaliases and converthtml5 scripts

--- bash-it/custom.bash
+++ bash-it/custom.bash
@@ -2,3 +2,6 @@
 function mcd() {
   mkdir -p "$1" && cd "$1";
 }
+source ~/.liquidprompt/liquidprompt
+source ~/.config/liquidpromptrc
+source ~/.fresh/build/shell.sh # source fresh rc for dotfile management
---
+++ shell/echoaliases.zsh
@@ -0,0 +1,13 @@
+_-accept-line () {
+    emulate -L zsh
+    local -a WORDS
+    WORDS=( ${(z)BUFFER} )
+    # Unfortunately ${${(z)BUFFER}[1]} works only for at least two words,
+    # thus I had to use additional variable WORDS here.
+    local -r FIRSTWORD=${WORDS[1]}
+    local -r GREEN=$'\e[32m' RESET_COLORS=$'\e[0m'
+    [[ "$(whence -w $FIRSTWORD 2>/dev/null)" == "${FIRSTWORD}: alias" ]] &&
+        echo -nE $'\n'"${GREEN}Executing $(whence $FIRSTWORD)${RESET_COLORS}"
+    zle .accept-line
+}
+zle -N accept-line _-accept-line
