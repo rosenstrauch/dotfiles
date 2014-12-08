c2d8da0d255896d866accc724e29a97a58ef2e17
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2014-12-08 03:45:58 +0100

    add huffshell

--- setup/gen-docs.zsh
+++ setup/gen-docs.zsh
@@ -5,18 +5,30 @@ setopt EXTENDED_GLOB
 # for file in "${ZDOTDIR:-$HOME}"/.dotfiles/setup/*.zsh; do
 # shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/setup/${file:h:t}.html"
 # done
-for file in "${ZDOTDIR:-$HOME}"/.dotfiles/setup/*.zsh; do
-  docco -L "${ZDOTDIR:-$HOME}"/.dotfiles/config/docco/languages.json ${file} -o "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}"
+# for file in "${ZDOTDIR:-$HOME}"/.dotfiles/setup/*.zsh; do
+#   docco -L "${ZDOTDIR:-$HOME}"/.dotfiles/config/docco/languages.json ${file} -o "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}"
+# done
+# 
+# 
+# for file in "${ZDOTDIR:-$HOME}"/.fresh/source/sorin-ionescu/prezto/modules/**/*.md; do
+#   shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/prezto/${file:h:t}.html"
+# done
+# 
+# 
+# for file in "${ZDOTDIR:-$HOME}"/.dotfiles/zsh/*; do
+#   shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/zsh/${file:t}.html"
+# done
+# 
+# 
+for file in "${ZDOTDIR:-$HOME}"/.fresh/source/**/**/*(#ia2)readme.*; do
+  mkdir ${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}
+  shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}/${file:h:t}.html"
 done
 
 
-for file in "${ZDOTDIR:-$HOME}"/.fresh/source/sorin-ionescu/prezto/modules/**/*.md; do
-  shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/prezto/${file:h:t}.html"
-done
 
+# Sources and their readmes
 
-for file in "${ZDOTDIR:-$HOME}"/.dotfiles/zsh/*; do
-  shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/zsh/${file:t}.html"
-done
+# commands and cheatsheets
 
-
+# configurations and settings
--- shell/zsh/aliases/aliases.zsh
+++ shell/zsh/aliases/aliases.zsh
@@ -1,7 +1,8 @@
 # aliases sourced from zshrc
 #alias git="git-achievements"
-alias fu="python ~/fu/fu"
+alias fu="python ~/.fresh/build/src/fu/fu"
 alias git-team="git log --format='%aN' | sort -u"
 alias ...='nocorrect cd ../../'
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+alias betty="~/.fresh/build/src/betty/main.rb"
