18f41f93dea69d7fc28b473c76c6482951046af9
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2015-06-27 22:32:15 +0200

    atom fontsize

--- setup/gen-docs.zsh
+++ setup/gen-docs.zsh
@@ -5,6 +5,7 @@ setopt EXTENDED_GLOB
 # for file in "${ZDOTDIR:-$HOME}"/.dotfiles/setup/*.zsh; do
 # shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/setup/${file:h:t}.html"
 # done
+
 # for file in "${ZDOTDIR:-$HOME}"/.dotfiles/setup/*.zsh; do
 #   docco -L "${ZDOTDIR:-$HOME}"/.dotfiles/config/docco/languages.json ${file} -o "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}"
 # done
@@ -19,10 +20,11 @@ setopt EXTENDED_GLOB
 #   shocco -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/zsh/${file:t}.html"
 # done
 #
-# find any readme files from fresh sources and create doc pages
+# find any readme files from fresh sources and convert to HTML
+
 for file in "${ZDOTDIR:-$HOME}"/.fresh/source/**/**/*(#ia2)readme.*; do
-  mkdir ${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}
-  shocco.sh -t "${file:t}" ${file} > "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}/${file:h:t}.html"
+  mkdir -p ${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}
+  pandoc ${file} -f markdown -t html -s -o "${ZDOTDIR:-$HOME}/.dotfiles/docs/${file:h:t}/${file:h:t}.html"
 done
 
 # System Hardware
