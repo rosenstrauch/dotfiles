edbe74d3b3ff000cbf419191ec469431433f5ca3
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2015-06-28 00:44:52 +0200

    make utils editable

--- shell/lib/utils.sh
+++ shell/lib/utils.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# References:
+# http://natelandau.com/bash-scripting-utilities/
+
 #
 # Set Colors
 #
@@ -128,26 +131,26 @@ pushover () {
 # of uninstalled items. Arrays in bash are insane (not in a good way).
 # Credit: https://github.com/cowboy/dotfiles
 #
-
-function to_install() {
-  local debug desired installed i desired_s installed_s remain
-  if [[ "$1" == 1 ]]; then debug=1; shift; fi
-    # Convert args to arrays, handling both space- and newline-separated lists.
-    read -ra desired < <(echo "$1" | tr '\n' ' ')
-    read -ra installed < <(echo "$2" | tr '\n' ' ')
-    # Sort desired and installed arrays.
-    unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
-      printf "%s\n" "${desired[@]}" | sort
-    )
-    unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
-      printf "%s\n" "${installed[@]}" | sort
-    )
-    # Get the difference. comm is awesome.
-    unset i; while read -r; do remain[i++]=$REPLY; done < <(
-      comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
-  )
-  [[ "$debug" ]] && for v in desired desired_s installed installed_s remain; do
-    echo "$v ($(eval echo "\${#$v[*]}")) $(eval echo "\${$v[*]}")"
-  done
-  echo "${remain[@]}"
-}
+#
+# function to_install() {
+#   local debug desired installed i desired_s installed_s remain
+#   if [[ "$1" == 1 ]]; then debug=1; shift; fi
+#     # Convert args to arrays, handling both space- and newline-separated lists.
+#     read -ra desired < <(echo "$1" | tr '\n' ' ')
+#     read -ra installed < <(echo "$2" | tr '\n' ' ')
+#     # Sort desired and installed arrays.
+#     unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
+#       printf "%s\n" "${desired[@]}" | sort
+#     )
+#     unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
+#       printf "%s\n" "${installed[@]}" | sort
+#     )
+#     # Get the difference. comm is awesome.
+#     unset i; while read -r; do remain[i++]=$REPLY; done < <(
+#       comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
+#   )
+#   [[ "$debug" ]] && for v in desired desired_s installed installed_s remain; do
+#     echo "$v ($(eval echo "\${#$v[*]}")) $(eval echo "\${$v[*]}")"
+#   done
+#   echo "${remain[@]}"
+# }
