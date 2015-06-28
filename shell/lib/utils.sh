8388f87e1fa1290a9a3d436124f494a7a76adb15
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2015-06-28 21:34:52 +0200

    add readonf to utils

--- shell/lib/utils.sh
+++ shell/lib/utils.sh
@@ -132,25 +132,36 @@ pushover () {
 # Credit: https://github.com/cowboy/dotfiles
 #
 #
-# function to_install() {
-#   local debug desired installed i desired_s installed_s remain
-#   if [[ "$1" == 1 ]]; then debug=1; shift; fi
-#     # Convert args to arrays, handling both space- and newline-separated lists.
-#     read -ra desired < <(echo "$1" | tr '\n' ' ')
-#     read -ra installed < <(echo "$2" | tr '\n' ' ')
-#     # Sort desired and installed arrays.
-#     unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
-#       printf "%s\n" "${desired[@]}" | sort
-#     )
-#     unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
-#       printf "%s\n" "${installed[@]}" | sort
-#     )
-#     # Get the difference. comm is awesome.
-#     unset i; while read -r; do remain[i++]=$REPLY; done < <(
-#       comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
-#   )
-#   [[ "$debug" ]] && for v in desired desired_s installed installed_s remain; do
-#     echo "$v ($(eval echo "\${#$v[*]}")) $(eval echo "\${$v[*]}")"
-#   done
-#   echo "${remain[@]}"
-# }
+function to_install() {
+  local debug desired installed i desired_s installed_s remain
+  if [[ "$1" == 1 ]]; then debug=1; shift; fi
+    # Convert args to arrays, handling both space- and newline-separated lists.
+    read -ra desired < <(echo "$1" | tr '\n' ' ')
+    read -ra installed < <(echo "$2" | tr '\n' ' ')
+    # Sort desired and installed arrays.
+    unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
+      printf "%s\n" "${desired[@]}" | sort
+    )
+    unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
+      printf "%s\n" "${installed[@]}" | sort
+    )
+    # Get the difference. comm is awesome.
+    unset i; while read -r; do remain[i++]=$REPLY; done < <(
+      comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
+  )
+  [[ "$debug" ]] && for v in desired desired_s installed installed_s remain; do
+    echo "$v ($(eval echo "\${#$v[*]}")) $(eval echo "\${$v[*]}")"
+  done
+  echo "${remain[@]}"
+}
+readconf () {
+  FILE=$1
+  while read -r line; do
+    # skip comments
+     [[ $line = \#* ]] && continue
+     # skip empty lines
+        -z "${line}"  && continue
+       eval ${line}
+     printf '%s\n' "$line"
+  done < "$FILE"
+}
