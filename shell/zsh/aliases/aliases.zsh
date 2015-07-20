77bf24b906872dcfe450553bf000067f93309eea
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2015-07-20 10:51:37 +0200

    prepare explainshell and thefuck

--- shell/zsh/aliases/aliases.zsh
+++ shell/zsh/aliases/aliases.zsh
@@ -10,3 +10,4 @@ alias zln='zmv -L'
 alias vg='vagrant'
 alias bty='~/betty/main.rb'
 alias drupalcs="phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme,js,css,info,txt'"
+alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
