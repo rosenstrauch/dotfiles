19863c8fa41266e7bafbf5bb49e5167a18041cce
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2016-06-17 00:22:39 +0200

    fork aliastips, codespell moved, add trello minor mode disable atom plugins add markdown mode to emacs; add emacs Keyboardshortcut C-d for duplicate line

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,12 +1,33 @@
 ;; User-setting area is below this line.
 
+;; Style
+
+
+(load-theme 'wheatgrass)
+
+
+;; Keyboard remappings
+
+;; Duplicate line http://stackoverflow.com/a/88828
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+)
+(global-set-key (kbd "C-d") 'duplicate-line)
+
+
 ;;
-;; Melpa for org-trello
+;; Melpa
 ;;
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (package-initialize)
 
@@ -17,24 +38,19 @@
 
 
 ;;
-;; Org-trello
+;; Markdown mode
 ;;
 
-(use-package org-trello
-  :ensure t)
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
-;; Custom list of trello org files
-(custom-set-variables '(org-trello-files '("~/org/boards/Welcome.trello")))
 
-;; org-trello major mode for all .trello files
-(add-to-list 'auto-mode-alist '("\\.trello$" . org-mode))
 
-;; add a hook function to check if this is trello file, then activate the org-trello minor mode.
-(add-hook 'org-mode-hook
-          (lambda ()
-            (let ((filename (buffer-file-name (current-buffer))))
-              (when (and filename (string= "trello" (file-name-extension filename)))
-              (org-trello-mode)))))
 ;;
 ;; ORG MODE
 ;;
@@ -57,8 +73,8 @@
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
 (setq org-agenda-files (quote ("~/org"
-"/run/media/rosenstrauch/DATA/DEV/Projectrepos/INTERNAL/rosenstrauch.com/WEBFACTORY/org"
-"/run/media/rosenstrauch/DATA/DEV/Projectrepos/INTERNAL/rosenstrauch.com/ROSENREPOS/org"
+"/DATA/01-INTERNAL/rosencom/WEBFACTORY/org"
+"/DATA/01-INTERNAL/rosencom/ROSENREPOS/org"
 )))
 
 ; Make tabs into spaces when you type them
@@ -75,15 +91,55 @@
    (quote
     ("/home/rosenstrauch/org/*.org"))))
 
+;; setup capture
+     (setq org-default-notes-file (concat org-directory "/capture.org"))
+     (define-key global-map "\C-cc" 'org-capture)
+
+;;
+;; Org-trello
+;;
+
+(use-package org-trello
+  :ensure t)
+
+;; Custom list of trello org files
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(org-agenda-files
+   (quote
+    ("/home/rosenstrauch/org/agendas.org" "/home/rosenstrauch/org/flagged.org" "/home/rosenstrauch/org/from-mobile.org" "/home/rosenstrauch/org/home.org" "/home/rosenstrauch/org/index.org")))
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
+ '(org-trello-files (quote ("~/org/boards/Welcome.trello")) nil (org-trello)))
+
+;; org-trello major mode for all .trello files
+(add-to-list 'auto-mode-alist '("\\.trello$" . org-mode))
+
+;; add a hook function to check if this is trello file, then activate the org-trello minor mode.
+(add-hook 'org-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name (current-buffer))))
+              (when (and filename (string= "trello" (file-name-extension filename)))
+              (org-trello-mode)))))
 ;; ORG-JIRA
+;; credentials are in authinfo
 (setq jiralib-url "http://acolono.atlassian.net")
 ;; you need make sure whether the "/jira" at the end is
 ;; necessary or not, see discussion at the end of this page
+(use-package org-jira
+  :ensure t)
 
-(require 'org-jira)
 ;; jiralib is not explicitly required, since org-jira will load it.
 
 
 ;; Youtrack
-(add-to-list 'load-path "~/.emacs.d/youtrack")
-(require 'youtrack)
+;;(add-to-list 'load-path "~/.emacs.d/youtrack")
+;;(require 'youtrack)
+
+;; GITLAB
+;; https://github.com/nlamirault/emacs-gitlab#usage
+(unless (package-installed-p 'gitlab)
+  (package-install 'gitlab))
