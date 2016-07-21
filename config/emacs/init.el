ff97d8230638fd69c8fc9233eb3d54956c998499
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2016-07-21 02:17:18 +0200

    annotate and extend emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,24 +1,40 @@
 ;; User-setting area is below this line.
 
+;; Global Emacs Settings
+
+; Make tabs into spaces when you type them
+(setq-default indent-tabs-mode nil)
+; Display existing tabs as 2 characters wide
+(setq-default tab-width 2)
+
+ ;; custom-set-variables
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+(custom-set-variables
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice "/home/rosenstrauch/org/home.org")
+ )
+
+
+;; Backups http://pages.sachachua.com/.emacs.d/Sacha.html#org7b1ada1
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+
+;;
 ;; Style
+;;
+
 (load-theme 'wheatgrass)
 
-;; TODO Keywords
-(setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+;;
+;; Global Emacs Keyboard remappings
+;;
 
-(setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "red" :weight bold)
-              ("NEXT" :foreground "blue" :weight bold)
-              ("DONE" :foreground "forest green" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
-              ("HOLD" :foreground "magenta" :weight bold)
-              ("CANCELLED" :foreground "forest green" :weight bold)
-              ("MEETING" :foreground "forest green" :weight bold)
-              ("PHONE" :foreground "forest green" :weight bold))))
-
-;; Keyboard remappings
 ;; Duplicate line http://stackoverflow.com/a/88828
 (defun duplicate-line()
   (interactive)
@@ -31,6 +47,12 @@
 )
 (global-set-key (kbd "C-d") 'duplicate-line)
 
+
+
+;; http://pages.sachachua.com/.emacs.d/Sacha.html#orgb362383
+(with-eval-after-load 'org
+     (bind-key "C-c k" 'org-cut-subtree org-mode-map)
+     (setq org-yank-adjusted-subtrees t))
 ;;
 ;; Melpa
 ;;
@@ -59,21 +81,129 @@
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
-
-
 ;;
 ;; ORG MODE
 ;;
 
+;; make emacs autorecognize org extension as org major mode
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; Install Org mode
 (use-package org
   :ensure t)
 
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+;; Configure org mode Directories
+(setq org-default-notes-file "~/org/home.org")
+(setq org-agenda-files "~/org/")
+(setq org-agenda-files
+      (append
+       (file-expand-wildcards "~/org/*/*.org")
+       (file-expand-wildcards "/mnt/DATA/02-CLIENTS/*/_ORG")
+       (file-expand-wildcards "/mnt/DATA/01-INTERNAL/*/org")))
+(setq org-archive-location "~/org/04-archive")
+
+
+;; Custom Agenda http://orgmode.org/worg/sources/org-tutorials/org-custom-agenda-commands.org
+(setq org-agenda-custom-commands
+
+
+      '(("Q" . "Custom queries") ;; gives label to "Q"
+	("Qa" "Archive search" search ""
+	 ((org-agenda-files (file-expand-wildcards "~/org/archive/*.org"))))
+	("Qw" "published search" search ""
+	 ((org-agenda-files (file-expand-wildcards "~/org/published/*.org"))))
+	("Qb" "published and Archive" search ""
+	 ((org-agenda-text-search-extra-files (file-expand-wildcards "~/archive/*.org"))))
+	        ;; searches both projects and archive directories
+	("QA" "Archive tags search" org-tags-view ""
+	 ((org-agenda-files (file-expand-wildcards "~/org/archive/*.org"))))
+	;; ...other commands here
+	 ))
+
+	 ;; Tags
+
+     (setq org-tag-alist '((:startgroup . nil)
+                           ("@work" . ?w) ("@home" . ?h)
+                           ("@errands" . ?t)
+                           ("@phone" . ?t)
+                           (:endgroup . nil)
+                           ("@PRJ" . ?e)
+                           ("laptop" . ?l) ("pc" . ?p)))
+
+;; Controlling tasks http://blog.aaronbieber.com/2016/01/30/dig-into-org-mode.html
+(setq org-log-redeadline (quote time))
+(setq org-log-done (quote time))
+(setq org-enforce-todo-dependencies t)
+(setq org-log-reschedule (quote time))
+
+;; TODO Keywords
+
+(setq org-todo-keywords
+      (quote ((sequence "WISH(w)" "INSERT(i)" "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("WISH" :foreground "medium sea green" :weight bold)
+              ("INSERT" :foreground "medium sea green" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+;; setup capture
+     (setq org-default-notes-file (concat org-directory "/capture.org"))
+     (define-key global-map "\C-cc" 'org-capture)
+
+;; Capture Templates http://orgmode.org/manual/Using-capture.html
+
+     (setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+             "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+             "* %?\nEntered on %U\n  %i\n  %a")
+        ("l" "Link" entry (file+datetree "~/org/links.org")
+                     "* %?\nEntered on %U\n  %i\n  %a")
+             ))
+
+;; Project tags http://juanreyero.com/article/emacs/org-teams.html
+
+(setq org-tags-exclude-from-inheritance '("@PRJ")
+      org-stuck-projects '("+@PRJ/-MAYBE-DONE"
+                           ("TODO" "TASK") ()))
+
+;; Org mode keyboard remappings
+
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(bind-key "C-c j" 'org-clock-goto) ;; jump to current task from anywhere
+(bind-key "C-c C-w" 'org-refile)
+
+;; Refile
+
+(setq org-refile-targets
+      '(
+      ("~/org/home.org" :maxlevel . 3)
+        ((file-expand-wildcards "~/org/06-teams/*.org") :maxlevel . 1)
+        ))
+(setq org-reverse-note-order t)
+(setq org-refile-use-outline-path nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+(setq org-refile-use-cache nil)
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
+(setq org-blank-before-new-entry nil)
+
+
+;; Mobile org
+
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/org")
 ;; Set to the name of the file where new notes will be stored
@@ -81,29 +211,7 @@
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
-(setq org-agenda-files (quote ("~/org"
-"/DATA/01-INTERNAL/rosencom/WEBFACTORY/org"
-"/DATA/01-INTERNAL/rosencom/ROSENREPOS/org"
-)))
 
-; Make tabs into spaces when you type them
-(setq-default indent-tabs-mode nil)
-; Display existing tabs as 2 characters wide
-(setq-default tab-width 2)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(initial-buffer-choice "/home/rosenstrauch/org/home.org")
- '(org-agenda-files
-   (quote
-    ("/home/rosenstrauch/org/*.org"))))
-
-;; setup capture
-     (setq org-default-notes-file (concat org-directory "/capture.org"))
-     (define-key global-map "\C-cc" 'org-capture)
 
 ;;
 ;; Org-trello
@@ -119,9 +227,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(org-agenda-files
-   (quote
-    ("/home/rosenstrauch/org/home.org" "/home/rosenstrauch/org/06-teams")))
+
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello)))
 
 ;; org-trello major mode for all .trello files
