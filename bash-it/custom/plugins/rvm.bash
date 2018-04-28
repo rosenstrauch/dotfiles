a046a175fb3e8768fc35454f3c2fe1cfd176bf05
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-04-28 21:06:21 +0200

    disable bashit rvm integration in case it interferes with tramp

--- bash-it/custom/plugins/rvm.bash
+++ bash-it/custom/plugins/rvm.bash
@@ -1,3 +1,12 @@
+# Disabled as test if it interferes with tramp
 # Load RVM into a shell session *as a function*
-[[ -s "${HOME}/.rvm/scripts/rvm" ]] && . "${HOME}/.rvm/scripts/rvm"
-export PATH="$PATH:$HOME/bin:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# [[ -s "${HOME}/.rvm/scripts/rvm" ]] && . "${HOME}/.rvm/scripts/rvm"
+# export PATH="$PATH:$HOME/bin:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+case "$TERM" in
+   "dumb")
+       export PS1="> "
+       ;;
+   xterm*|rxvt*|eterm*|screen*)
+       tty -s && export PS1="some crazy prompt stuff"
+       ;;
+esac
