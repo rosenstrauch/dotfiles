6a4816641730f829ac04a0a4a24e9a17694f7737
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-05-07 21:50:56 +0200

    add basic php support to emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -307,6 +307,39 @@
   :init (global-flycheck-mode)
   )
 
+;;; * Use Package php Mode [#2]
+(use-package php-mode
+  :ensure t)
+
+(use-package phpcbf
+  :ensure t
+  :init
+   (setq phpcbf-executable "~/bin/phpcbf")
+  :config
+  (add-hook 'php-mode-hook 'phpcbf-enable-on-save)
+  (setq phpcbf-standard "Drupal")
+  )
+
+;; (use-package flymake-php
+;;   :ensure t
+;;   :config
+;;   (add-hook 'php-mode-hook 'flymake-php-load))
+
+;; (use-package flymake-phpcs
+;;   :ensure t
+;;   :init
+;;    (setq flymake-phpcs-standard "Drupal")
+;;   :config
+
+
+;; ;; Customize the coding standard checked by phpcs
+;;   (setq flymake-phpcs-standard
+;;         "~/.fresh/build/src/PHP_CodeSniffer/CodeSniffer/Standards/Drupal")
+
+;; ;; Show the name of sniffs in warnings (eg show
+;; ;; "Generic.CodeAnalysis.VariableAnalysis.UnusedVariable" in an unused
+;; ;; variable warning)
+;; (setq flymake-phpcs-show-rule t))
 
 ;;; * Use Package: Org Trello [#3]
 (use-package org-trello
