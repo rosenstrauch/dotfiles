9160db4c30487ef5d2cd509541dcd5b199c287b0
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-06-19 06:28:01 +0200

    js session

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -322,8 +322,21 @@
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode)
-  )
+	:config
+	;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
+  )
 ;;; * Use Package php Mode [#2]
 (use-package php-mode
   :ensure t)
