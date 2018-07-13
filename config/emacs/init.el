b7ce9592fcb5e27a4bfdd0b822373af182a23117
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-07-13 17:25:27 +0200

    configure bitlbee

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -143,10 +143,40 @@
 
 ;;; * irc
 (use-package erc
-  :commands (erc-tls erc-next-channel-buffer my-erc-md-all-but-emacs)
+  :commands (erc-tls erc-next-channel-buffer my-erc-md-all-but-emacs bitlbee)
   :init
 
-
+            (defun bitlbee ()
+              (interactive)
+              (if (get-buffer "&bitlbee")
+                  (switch-to-buffer (get-buffer "&bitlbee"))
+                (let ((default-directory "~"))
+                  (erc :server "localhost" :port 6667 :nick "rosenstrauch"))))
+            
+  (defun bitlbee-netrc-identify ()
+    "Auto-identify for Bitlbee channels using authinfo or netrc.
+    
+    The entries that we look for in netrc or authinfo files have
+    their 'port' set to 'bitlbee', their 'login' or 'user' set to
+    the current nickname and 'server' set to the current IRC
+    server's name.  A sample value that works for authenticating
+    as user 'keramida' on server 'localhost' is:
+    
+    machine localhost port bitlbee login keramida password supersecret"
+    (interactive)
+    (when (string= (buffer-name) "&bitlbee")
+      (let* ((secret (plist-get (nth 0 (auth-source-search :max 1
+							   :host erc-server
+							   :user (erc-current-nick)
+							   :port "bitlbee"))
+				:secret))
+	     (password (if (functionp secret)
+			   (funcall secret)
+			 secret)))
+	(erc-message "PRIVMSG" (concat (erc-default-target) " " "identify" " " password) nil))))
+  
+  ;; Enable the netrc authentication function for &biblbee channels.
+  (add-hook 'erc-join-hook 'bitlbee-netrc-identify)
 
 ;;; minimal distraction everywhere except my own channel
   (defun my-erc-md-all-but-emacs ()
