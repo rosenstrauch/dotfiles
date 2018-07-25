3f02cd4abbbe9d39f1ae5158a840a121d1ed683e
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-07-25 23:17:41 +0200

    add systemd units

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -271,10 +271,11 @@
          ("r" . notmuch-search-reply-to-thread)
          ("R" . notmuch-search-reply-to-thread-sender)
          ("d" . notmuch-search-filter-by-date))
-         :config
+  :config
   (progn
     (setq notmuch-archive-tags '("-inbox" "-unread" "+archive"))
-    (setq notmuch-message-headers '("To" "Cc" "Subject" "Date"))))
+    (setq notmuch-message-headers '("To" "Cc" "Subject" "Date")))
+  )
 
 ;;; * Use package: babel restclient
 (use-package ob-restclient
@@ -487,6 +488,70 @@
 (use-package js-import
   :ensure t)
 
+
+;;; * Use Package Python
+
+
+(use-package python-mode
+  :mode "\\.py\\'"
+  :interpreter "python"
+  :bind (:map python-mode-map
+              ("C-c c")
+              ("C-c C-z" . python-shell))
+  :config
+  (defvar python-mode-initialized nil)
+
+  (defun my-python-mode-hook ()
+    (unless python-mode-initialized
+      (setq python-mode-initialized t)
+
+      (info-lookup-add-help
+       :mode 'python-mode
+       :regexp "[a-zA-Z_0-9.]+"
+       :doc-spec
+       '(("(python)Python Module Index" )
+         ("(python)Index"
+          (lambda
+            (item)
+            (cond
+             ((string-match
+               "\\([A-Za-z0-9_]+\\)() (in module \\([A-Za-z0-9_.]+\\))" item)
+              (format "%s.%s" (match-string 2 item)
+                      (match-string 1 item)))))))))
+
+    (set (make-local-variable 'parens-require-spaces) nil)
+    (setq indent-tabs-mode nil))
+
+  (add-hook 'python-mode-hook 'my-python-mode-hook)
+  (add-hook 'python-mode-hook 'smartparens-mode)
+  (add-hook 'python-mode-hook 'color-identifiers-mode)
+  )
+(use-package pyenv-mode
+  :ensure t
+  :config
+    (defun projectile-pyenv-mode-set ()
+      "Set pyenv version matching project name."
+      (let ((project (projectile-project-name)))
+        (if (member project (pyenv-mode-versions))
+            (pyenv-mode-set project)
+          (pyenv-mode-unset))))
+
+    (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set)
+    (add-hook 'python-mode-hook 'pyenv-mode))
+(use-package pyenv-mode-auto
+  :ensure t)
+(use-package jedi
+  :ensure nil
+  :disabled t
+  :init
+  (add-to-list 'company-backends 'company-jedi)
+  :config
+  (use-package company-jedi
+    :ensure nil
+    :disabled t
+    :init
+    (add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))
+    (setq company-jedi-python-bin "python")))
 ;;; * Use Package Elm Mode [#2]
 
 (use-package elm-mode
