8159b5eb54c611af30d28b12d17988932e69ff02
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-07-26 19:29:02 +0200

    update paths to maildirs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -69,17 +69,7 @@
 (global-display-line-numbers-mode t)
 (column-number-mode 1)
 (setq line-spacing '0.25)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((((background light)) (:background "black")) (((background dark)) (:background "white"))))
- '(org-level-1 ((t (:inherit outline-1 :background "blue" :foreground "lavender blush" :box nil :height 1.4))))
- '(org-level-2 ((t (:inherit outline-2 :foreground "orange" :box nil :height 1.2))))
- '(org-level-3 ((t (:inherit outline-3 :foreground "magenta" :box nil :height 0.9))))
- '(org-level-4 ((t (:inherit outline-4 :foreground "khaki" :box nil :height 0.7))))
- '(org-level-5 ((t (:inherit outline-5 :foreground "orchid1" :box nil :height 0.5)))))
+
 
 ;;; ** CUSTOM functions
 ;;; *** CUSTOM open emacs config
@@ -270,11 +260,8 @@
          ;; bind 'r' to reply-all, and 'R' to reply
          ("r" . notmuch-search-reply-to-thread)
          ("R" . notmuch-search-reply-to-thread-sender)
-         ("d" . notmuch-search-filter-by-date))
-  :config
-  (progn
-    (setq notmuch-archive-tags '("-inbox" "-unread" "+archive"))
-    (setq notmuch-message-headers '("To" "Cc" "Subject" "Date")))
+         ("§" . notmuch-search-filter-by-date))
+
   )
 
 ;;; * Use package: babel restclient
--- config/emacs/notmuch-config.el
+++ config/emacs/notmuch-config.el
@@ -5,15 +5,7 @@
 
 ;;; Code:
 (setq notmuch-archive-tags '("-unread" "+archive" "-inbox"))
-(setq notmuch-saved-searches '(
-                               (:name "notifications" :query "tag:notifications")
-                               (:name "Y2018" :query "date:2017..2018")t
-			                         (:name "unread" :query "tag:unread" :key "u")
-			                         (:name "flagged" :query "tag:flagged" :key "f")
-			                         (:name "sent" :query "tag:sent" :key "t")
-			                         (:name "drafts" :query "tag:draft" :key "d")
-			                         (:name "all mail" :query "*" :key "a")
-                               (:name "inbox" :query "tag:inbox" :count-query "tag:inbox and tag:unread" :sort-order oldest-first :key "i")))
+
 
 (provide 'notmuch-config)
 ;;; notmuch-config.el ends here
