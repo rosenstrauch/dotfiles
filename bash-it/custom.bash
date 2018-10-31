af076586c65b6841aea6f96e3bce0d8865b98467
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-10-31 14:39:49 +0100

    refactor: freshrc file order

--- bash-it/custom.bash
+++ bash-it/custom.bash
@@ -4,3 +4,4 @@ source ~/.config/liquidpromptrc
 source ~/.fresh/build/shell.sh # source fresh rc for dotfile management
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+export PATH="$(yarn global bin):$PATH"
