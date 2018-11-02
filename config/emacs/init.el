eda7c85c3e8c2d9eb18b7218b3bcc280fbef63e8
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-11-02 15:32:11 +0100

    fix: magit configuration

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -408,18 +408,43 @@
     (package-install 'gitlab))
 
 ;;; * Use Package: Magit [#11]
-  (use-package magit
-    :bind ("C-x g" . magit-status)
-    :config
-    (progn
-      (setenv "GIT_PAGER" "")
-      (add-hook 'magit-log-edit-mode-hook
-                '(lambda ()
-                   (auto-fill-mode)
-                   (flyspell-mode)
-                   (set-fill-column 80)))))
-  ;; optional keyboard short-cut
-  (global-set-key "\C-xm" 'browse-url-at-point)
+(use-package magit
+  :commands (magit-status magit-blame magit-mode)
+  :ensure t
+  :bind (("C-x g" . magit-status)
+         ("C-c C-g l" . magit-file-log)
+         ("C-c C-g c" . magit-commit)
+         ("C-c C-g f" . magit-grep))
+  :config
+  (progn
+    ;; Set `magit-status' fullscreen
+    (setq magit-post-display-buffer-hook
+          #'(lambda ()
+              (when (derived-mode-p 'magit-status-mode)
+                (delete-other-windows))))
+
+    (setenv "GIT_PAGER" "")
+    (add-hook 'magit-log-edit-mode-hook
+              '(lambda ()
+                 (auto-fill-mode)
+                 (flyspell-mode)
+                 (set-fill-column 80))))
+  (use-package magit-blame
+    :bind ("C-c C-g b" . magit-blame-mode))
+ )
+
+(use-package magit-gh-pulls
+  :diminish magit-gh-pulls-mode
+  :ensure t
+  :disabled t
+  ;;:hook       ('magit-mode-hook 'turn-on-magit-gh-pulls)
+  :commands (magit-gh-pulls-mode turn-on-magit-gh-pulls))
+(use-package magithub
+  :after magit
+  :ensure t
+  :disabled t
+  :config (magithub-feature-autoinject t))
+
   :ensure t)
 
 (use-package ghub
