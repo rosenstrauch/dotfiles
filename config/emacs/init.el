7c81fe0a52a878b657c1e14e888f3919248850e2
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2018-12-29 13:44:00 +0100

    update docksal

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -487,6 +487,19 @@
 Cc:
 X-Message-SMTP-Method: sendmail
 ")
+          ;; This is needed to allow msmtp to do its magic:
+(setq message-sendmail-f-is-evil 't)
+
+;;need to tell msmtp which account we're using
+(setq message-sendmail-extra-arguments '("--read-envelope-from"))
+(setq mail-specify-envelope-from t)
+(setq mail-envelope-from 'header)
+(setq message-sendmail-envelope-from 'header)
+(setq message-send-mail-function 'message-send-mail-with-sendmail)
+
+;;use msmtp instead of sendmail
+(setq sendmail-program "/usr/bin/msmtp")
+
           (setq message-kill-buffer-on-exit t)
           (setq message-alternative-emails
                 (regexp-opt '("green@endeavour.localdomain" "luis.rosenstrauch@gmail.com")))
